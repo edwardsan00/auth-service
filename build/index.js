db535023c5438f419e94cbf8ba27cc37f9b1aa69
@edwardsan00 <edwardsan00@gmail.com>
2020-05-02 21:34:16 -0500

    [ UPDATE ] - build ignore

--- build/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-var app = express();
-app.get('/', function (req, resp) {
-    resp.send('Hello get server');
-});
-app.listen(3000, function () {
-    console.log('Corriendo en el puerto 3000');
-});
